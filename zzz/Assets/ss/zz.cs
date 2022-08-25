329848e05709aeaf8e64b28eedadf03aa6152df9
فواز1231 <fawaz1005d@gmail.com>
2022-08-26 02:38:45 +0300

    xz

---
+++ zzz/Assets/ss/zz.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zz : MonoBehaviour
+{
+    public Transform min;
+    public Transform max;
+    public int inc;
+    public AudioSource src;
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+      Move();
+   } 
+
+    void Move()
+   {
+      if (Input.GetKeyDown(KeyCode.D))
+      {
+         transform.position = new Vector3(Mathf.Clamp(transform.position.x +  inc , min.position.x, max.position.x), transform.position.y, transform.position.z);
+      }
+    
+      if (Input.GetKeyDown(KeyCode.A))
+      {
+         transform.position = new Vector3(Mathf.Clamp(transform.position.x - inc , min.position.x, max.position.x), transform.position.y, transform.position.z);
+      }
+  }  
+   private void OnCollisionEnter (Collision Collision) 
+  {
+     if (Collision.gameObject.tag == "Enemy")
+    {
+      src.Play();
+      Invoke("Restart", 1f);
+    }   
+  }
+  void Restart() 
+  {
+    SceneManager.LoadScene(0); 
+  }
+  
+   
+}
